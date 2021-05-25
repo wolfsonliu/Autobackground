9cadd2f7fdc79813e6f7be512201eaa672c29833
wolfsonliu <wolfsonliu@live.com>
2021-05-25 17:37:05 +0800

    modified MacOS script

--- bing_daily_osx.sh
+++ bing_daily_osx.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
-BG_DIR=$HOME/Pictures/Wallpapers
+BG_DIR=$HOME/Pictures/bing
 BING_ADDR="https://www.bing.com"
 JSON_LINK="http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
 JSON=$(curl ${JSON_LINK} 2>/dev/null)
 
 FIG_LINK=${BING_ADDR}$(echo ${JSON} | tr ',' '\n' | grep \"url\" | cut -d: -f 2 | tr -d '"')
-FIG_NAME=${FIG_LINK##*/}
+FIG_NAME=$(echo ${FIG_LINK##*/} | sed "s/th?id=OHR.//g" | sed -E "s/&rf=.*$//g")
 FIG_PATH=${BG_DIR}/${FIG_NAME}
 
 if [ ! -e ${FIG_PATH} ]; then
-    wget -q ${FIG_LINK} -O ${FIG_PATH}
+    /usr/local/bin/wget -q ${FIG_LINK} -O ${FIG_PATH}
+fi
+
+if [ -e ${FIG_PATH} ]; then
     SCRIPT='tell application "Finder" to set desktop picture to POSIX file '\"${FIG_PATH}\"
     osascript -e "${SCRIPT}"
 fi
